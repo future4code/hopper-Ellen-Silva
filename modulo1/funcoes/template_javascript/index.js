900b19b3e3ef5cdda9497b04f1e33db38340aa57
ellen-ls <ellen.l.s1996@gmail.com>
2022-02-17 21:58:22 -0300

    funcao

---
+++ modulo1/funcoes/template_javascript/index.js
@@ -0,0 +1,120 @@
+//Exercícios de interpretação de código
+
+//Questão 1
+/*function minhaFuncao(variavel) {
+	return variavel * 5
+}
+
+console.log(minhaFuncao(2))
+console.log(minhaFuncao(10))
+
+//a. vai dar valor 10 no primeiro e 50 no segundo
+//b. não irá retornar nada
+
+//Questão 2
+/*let textoDoUsuario = prompt("Insira um texto");
+
+const outraFuncao = function(texto) {
+	return texto.toLowerCase().includes("cenoura")
+}
+
+const resposta = outraFuncao(textoDoUsuario)
+console.log(resposta)*/
+//a. vai deixar todas as letras em minusculo e se tiver um nome cenoura na frase, vai vir verdadeiro
+//b.todas irão dar true por que é cenoura, nao importa se tem o "s" ou esta em maiusculo
+
+//Exercícios de escrita de código
+
+//Questão 1
+//a. 
+/*const nome = "Ellen"
+const idade = "25"
+const cidade = "Fortaleza"
+const profissão = "Auxiliar de farmacia"
+
+function pessoa(){
+
+    return pessoa
+
+}
+console.log(`Meu nome é ${nome}, tenho ${idade}, moro em ${cidade} e sou ${profissão}`)
+
+//b. 
+function pessoa1(name,age,city,profession){
+    console.log(`Eu sou ${name}, tenho ${age}, moro em ${city} e sou ${profession}`)
+
+   
+}
+pessoa1("Ellen", 25 , "Fortaleza", "Auxiliar de farmacia" )*/
+
+//Questão 2
+//a.
+/*function soma(parametro1, parametro2){
+let soma1 = parametro1+parametro2
+
+return soma1
+}
+console.log( soma( 2, 5))*/
+
+//b.
+/*function maiorQue( numero1, numero2){
+
+let numero = numero1 >= numero2
+
+return numero
+}
+console.log(maiorQue(8,10))*/
+
+//c.
+/*function parOuImpar(numero){
+
+let numeroPar = numero
+
+return numeroPar
+}
+console.log(parOuImpar(10%2===0))*/
+
+//d.
+
+/*function string(frase){
+  console.log( frase.length, frase.toUpperCase())
+
+    
+}
+const texto =  "I'm very tired, I need to a new job"
+console.log(string(texto))
+
+//Questão 3
+/*let retorno= Number (prompt("digite um numero"))
+let retorno1 = Number (prompt("digite outro numero"))
+
+function things(soma){
+
+   let soma1= retorno+ retorno1
+      
+   return soma1
+  
+}
+console.log("soma:",things())
+
+function things1(subtracao){
+     let subtracao1= retorno-retorno1
+     return subtracao1
+}
+console.log("subtrção:",things1())
+
+function things2(multiplicacao){
+    let multiplicacao1= retorno*retorno1
+    return multiplicacao1
+}
+console.log("multiplicação:",things2())
+
+function things3(divisao){
+    let divisao1= retorno/retorno1
+    return divisao1
+}
+console.log("divisao:",things3())*/
+
+
+
+
