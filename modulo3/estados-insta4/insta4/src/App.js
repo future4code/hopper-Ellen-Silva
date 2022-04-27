5cbc2990e8d3f873053f93dd304160360e758cca
ellen-ls <ellen.l.s1996@gmail.com>
2022-04-26 21:57:06 -0300

    projeto-insta4

---
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from 'styled-components'
+import Post from './components/Post/Post';
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+
+class App extends React.Component {
+  render() {
+    return (
+      <MainContainer>
+        <Post
+          nomeUsuario={'paulinha'}
+          fotoUsuario={'https://picsum.photos/50/48'}
+          fotoPost={'https://picsum.photos/200/147'}
+        />
+
+        <Post
+          nomeUsuario={'Ana'}
+          fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/200/145'}
+        />
+
+        <Post
+          nomeUsuario={'DeBemComaVida'}
+          fotoUsuario={'https://picsum.photos/50/49'}
+          fotoPost={'https://picsum.photos/200/148'}
+        />
+      </MainContainer>
+
+
+    );
+  }
+}
+
+export default App;
