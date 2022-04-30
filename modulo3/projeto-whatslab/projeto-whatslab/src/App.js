b3d62b29c4a7d9e31710d0c5830d7edc8d2fdd41
ellen-ls <ellen.l.s1996@gmail.com>
2022-04-29 21:15:30 -0300

    fazer fds

---
+++ modulo3/projeto-whatslab/projeto-whatslab/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <input></input>
+      <input></input>
+      <button>Enviar</button>
+
+    </div>
+  );
+}
+
+export default App;
