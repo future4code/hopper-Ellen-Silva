2147697589e32dab6bd11c532b42e89e54c25348
ellen-ls <ellen.l.s1996@gmail.com>
2022-05-19 23:30:05 -0300

    terminar amanha

---
+++ modulo3/apis-labenusers/apis-labenusers/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import IntegracaoAPI from './integracaoAPI';
+
+function App() {
+  return (
+    <div className="App">
+      <p></p>
+      <IntegracaoAPI>
+        
+      </IntegracaoAPI>
+      
+    </div>
+  );
+}
+
+export default App;
