d05d5a965978accbaaacc99d8299c84d3c2aed51
ellen-ls <ellen.l.s1996@gmail.com>
2022-05-29 23:14:54 -0300

    projeto-não finalizado totalmente

---
+++ modulo3/projeto-labefy/src/components/VerPlaylist.js
@@ -0,0 +1,17 @@
+import React from "react";
+import axios from "axios";
+
+class VerPlaylist extends React.Component{
+
+    render(){
+        return(
+            <div>Detalhes Playlist
+                <button onClick={0}>Voltar</button>
+            </div>
+            
+        )
+    }
+}
+
+
+export default VerPlaylist;
