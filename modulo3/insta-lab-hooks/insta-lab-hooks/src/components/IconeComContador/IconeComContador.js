829c2fb50d75696c58ecd59936522daafef3be5c
ellen-ls <ellen.l.s1996@gmail.com>
2022-05-31 22:03:11 -0300

    useState

---
+++ modulo3/insta-lab-hooks/insta-lab-hooks/src/components/IconeComContador/IconeComContador.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from "styled-components"
+
+const IconeContainer = styled.div`
+	display: flex;
+
+	img {
+		margin-right: 5px;
+	}
+`
+
+function IconeComContador(props) {
+	return <IconeContainer>
+		<img alt={'Icone'} src={props.icone} onClick={props.onClickIcone}/>
+		<p>{props.valorContador}</p>
+		{props.iconeCurtida}
+		
+		
+		{props.numeroCurtidas}
+		{props.numeroComentarios} 
+
+	</IconeContainer>
+}
+
+export default IconeComContador
