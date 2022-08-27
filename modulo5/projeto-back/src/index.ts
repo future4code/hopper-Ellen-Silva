d70385a4dca99583232da48b6439df25f7640664
ellen-ls <ellen.l.s1996@gmail.com>
2022-08-27 20:01:52 -0300

    projeto nao finalizado

---
+++ modulo5/projeto-back/src/index.ts
@@ -0,0 +1,80 @@
+import express, {Express, Request, Response} from 'express'
+import cors from 'cors'
+import { AddressInfo } from "net";
+import { type, userInfo } from 'os';
+import { User, users } from './data';
+
+const app: Express = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.post('/users', (req: Request, res: Response)=>{
+  let errorCode = 400
+  try{
+   const {name, cpf, birthday, balance, extract} = req.body
+   
+   if(!name ||!cpf || !birthday){
+      errorCode = 422
+      throw new Error("Preencha todas as informações corretamente");
+}
+
+const date = new Date();
+const formatedDate = Number(date.getDate() + '/' + (date.getMonth()+1) + '/' + date.getFullYear())
+const birthDate = Number(birthday)
+const age = (formatedDate - birthDate)
+   
+   if(age < 18){
+    errorCode = 403
+    throw new Error("Idade não permetida, sua idade é inferior a 18 anos");
+    
+   }
+
+  const user = users.find((c)=>{
+    return c.cpf === cpf
+
+  })
+   
+  if(user){
+    errorCode = 409
+    throw new Error("CPF ja cadastrado");
+    
+  }
+
+ 
+    const newUser : User = {
+      id: Math.random(),
+      name: name,
+      cpf: cpf,
+      birthday: birthday,
+      balance: balance,
+      extract:extract,
+    }
+
+    users.push(newUser)
+
+    res.status(errorCode).send({message: "Usuário criado com sucesso!", users})
+      
+  }catch (error:any){
+   res.status(errorCode).send(error.message)
+
+  }
+})
+
+app.get('users/saldo', (req:Request , res:Response)=>{
+
+  try{
+
+  }catch (error:any){
+    
+  }
+})
+
+const server = app.listen(process.env.PORT || 3003, () => {
+    if (server) {
+       const address = server.address() as AddressInfo;
+       console.log(`Server is running in http://localhost: ${address.port}`);
+    } else {
+       console.error(`Failure upon starting server.`);
+    }
+});
